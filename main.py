3b616919964832cc457094970b60c051989fc4be
Gouravsaha12 <119443395+Gouravsaha12@users.noreply.github.com>
2025-03-15 21:55:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -161,4 +161,4 @@ async def get_film_history(email: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
